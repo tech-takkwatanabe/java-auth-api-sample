5b6c9d38ba8e1584a118cd68156e7b27a3dda3d2
tech-takkwatanabe <tech.takkwatanabe@gmail.com>
2025-05-16 19:17:46 +0900

    ✨ TypeHandlerを追加してUUIDのマッピングを修正

---
+++ apps/api/src/main/java/com/example/api/common/mybatis/UUIDTypeHandler.java
@@ -0,0 +1,33 @@
+package com.example.api.common.mybatis;
+
+import com.example.api.auth.domain.vo.UUID;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.sql.*;
+
+public class UUIDTypeHandler extends BaseTypeHandler<UUID> {
+
+  @Override
+  public void setNonNullParameter(PreparedStatement ps, int i, UUID parameter, JdbcType jdbcType) throws SQLException {
+    ps.setObject(i, parameter.getValue());
+  }
+
+  @Override
+  public UUID getNullableResult(ResultSet rs, String columnName) throws SQLException {
+    Object value = rs.getObject(columnName);
+    return value != null ? UUID.from((String) value) : null;
+  }
+
+  @Override
+  public UUID getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+    Object value = rs.getObject(columnIndex);
+    return value != null ? UUID.from((String) value) : null;
+  }
+
+  @Override
+  public UUID getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+    Object value = cs.getObject(columnIndex);
+    return value != null ? UUID.from((String) value) : null;
+  }
+}
