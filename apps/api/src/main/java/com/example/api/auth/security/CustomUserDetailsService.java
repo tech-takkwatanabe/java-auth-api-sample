c379cd549e7862ad65135a3e1041a3a92240a37e
tech-takkwatanabe <tech.takkwatanabe@gmail.com>
2025-05-23 18:06:02 +0900

    🎨 Emailでログインする修正

--- apps/api/src/main/java/com/example/api/auth/security/CustomUserDetailsService.java
+++ apps/api/src/main/java/com/example/api/auth/security/CustomUserDetailsService.java
@@ -2,7 +2,6 @@ package com.example.api.auth.security;
 
 import com.example.api.auth.domain.repository.UserRepository;
 import com.example.api.auth.model.User;
-
 import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,10 +19,11 @@ public class CustomUserDetailsService implements UserDetailsService {
   public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
     User user = userRepository
         .findByEmail(email)
-        .orElseThrow(
-            () -> new UsernameNotFoundException("User not found with email: " + email));
+        .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
 
     return new org.springframework.security.core.userdetails.User(
-        user.getEmail(), user.getPassword(), Collections.emptyList());
+        user.getEmail(),
+        user.getPassword(),
+        Collections.emptyList());
   }
 }
