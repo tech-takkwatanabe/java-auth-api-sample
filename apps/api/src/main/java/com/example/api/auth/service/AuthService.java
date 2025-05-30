347e473e24f1dd9f2c68618c7d0644a60920a5f7
tech-takkwatanabe <tech.takkwatanabe@gmail.com>
2025-05-30 11:05:08 +0900

    🎨 Formatting

--- apps/api/src/main/java/com/example/api/auth/service/AuthService.java
+++ apps/api/src/main/java/com/example/api/auth/service/AuthService.java
@@ -7,15 +7,18 @@ import com.example.api.auth.domain.dto.response.JwtResponse;
 import com.example.api.auth.domain.dto.response.MessageResponse;
 import com.example.api.auth.domain.dto.response.TokenRefreshResponse;
 import com.example.api.auth.domain.dto.response.UserResponse;
+import com.example.api.auth.domain.entity.RefreshToken;
 import com.example.api.auth.domain.repository.RefreshTokenRepository;
 import com.example.api.auth.domain.vo.UUID;
-import com.example.api.auth.domain.entity.RefreshToken;
 import com.example.api.auth.exception.TokenRefreshException;
 import com.example.api.auth.mapper.UserMapper;
 import com.example.api.auth.model.User;
 import com.example.api.auth.security.JwtTokenProvider;
-
+import com.example.api.auth.util.JwtUtils;
 import jakarta.servlet.http.HttpServletRequest;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -25,11 +28,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.util.Optional;
-import com.example.api.auth.util.JwtUtils;
 
 @Service
 @RequiredArgsConstructor
@@ -51,14 +49,15 @@ public class AuthService {
       return MessageResponse.builder().message("Error: Email is already in use!").build();
     }
 
-    User user = User.builder()
-        .uuid(UUID.randomUUID())
-        .username(signupRequest.getUsername())
-        .email(signupRequest.getEmail())
-        .password(passwordEncoder.encode(signupRequest.getPassword()))
-        .createdAt(LocalDateTime.now())
-        .updatedAt(LocalDateTime.now())
-        .build();
+    User user =
+        User.builder()
+            .uuid(UUID.randomUUID())
+            .username(signupRequest.getUsername())
+            .email(signupRequest.getEmail())
+            .password(passwordEncoder.encode(signupRequest.getPassword()))
+            .createdAt(LocalDateTime.now())
+            .updatedAt(LocalDateTime.now())
+            .build();
 
     userMapper.insert(user);
     return MessageResponse.builder().message("User registered successfully!").build();
@@ -66,28 +65,31 @@ public class AuthService {
 
   @Transactional
   public JwtResponse authenticateUser(LoginRequest loginRequest) {
-    Authentication authentication = authenticationManager.authenticate(
-        new UsernamePasswordAuthenticationToken(
-            loginRequest.getEmail(), loginRequest.getPassword()));
+    Authentication authentication =
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                loginRequest.getEmail(), loginRequest.getPassword()));
 
     SecurityContextHolder.getContext().setAuthentication(authentication);
 
     String email = authentication.getName();
-    User user = Optional.ofNullable(userMapper.selectByEmail(email))
-        .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+    User user =
+        Optional.ofNullable(userMapper.selectByEmail(email))
+            .orElseThrow(
+                () -> new UsernameNotFoundException("User not found with email: " + email));
 
     refreshTokenRepository.deleteByUuid(user.getUuid());
 
     String accessToken = jwtTokenProvider.generateAccessToken(user.getUuid().getValue());
     String refreshToken = jwtTokenProvider.generateRefreshToken(user.getUuid().getValue());
 
-    RefreshToken entityRefreshToken = RefreshToken
-        .builder()
-        .userUuid(user.getUuid().toString())
-        .token(refreshToken)
-        .expiryDate(Instant.now().plus(jwtTokenProvider.getRefreshTokenDuration()))
-        .revoked(false)
-        .build();
+    RefreshToken entityRefreshToken =
+        RefreshToken.builder()
+            .userUuid(user.getUuid().toString())
+            .token(refreshToken)
+            .expiryDate(Instant.now().plus(jwtTokenProvider.getRefreshTokenDuration()))
+            .revoked(false)
+            .build();
 
     refreshTokenRepository.save(entityRefreshToken);
 
@@ -106,8 +108,11 @@ public class AuthService {
     String requestRefreshToken = request.getRefreshToken();
 
     UUID userUuid = new UUID(jwtTokenProvider.getUserUuidFromToken(requestRefreshToken));
-    RefreshToken token = refreshTokenRepository.findByUuid(userUuid)
-        .orElseThrow(() -> new TokenRefreshException(requestRefreshToken, "Refresh token not found!"));
+    RefreshToken token =
+        refreshTokenRepository
+            .findByUuid(userUuid)
+            .orElseThrow(
+                () -> new TokenRefreshException(requestRefreshToken, "Refresh token not found!"));
 
     if (!token.getToken().equals(requestRefreshToken)) {
       throw new TokenRefreshException(requestRefreshToken, "Refresh token is invalid!");
@@ -123,11 +128,11 @@ public class AuthService {
         .build();
   }
 
-  private RefreshToken verifyExpiration(
-      RefreshToken token) {
+  private RefreshToken verifyExpiration(RefreshToken token) {
     if (token.isRevoked() || token.getExpiryDate().isBefore(Instant.now())) {
       refreshTokenRepository.deleteByUuid(UUID.from(token.getUserUuid()));
-      throw new TokenRefreshException(token.getToken(), "Refresh token was expired or revoked. Please login again.");
+      throw new TokenRefreshException(
+          token.getToken(), "Refresh token was expired or revoked. Please login again.");
     }
     return token;
   }
@@ -140,8 +145,10 @@ public class AuthService {
     }
 
     UUID userUuid = new UUID(jwtTokenProvider.getUserUuidFromToken(token));
-    User user = Optional.ofNullable(userMapper.selectByUuid(userUuid.toString()))
-        .orElseThrow(() -> new UsernameNotFoundException("User not found with uuid: " + userUuid));
+    User user =
+        Optional.ofNullable(userMapper.selectByUuid(userUuid.toString()))
+            .orElseThrow(
+                () -> new UsernameNotFoundException("User not found with uuid: " + userUuid));
 
     refreshTokenRepository.deleteByUuid(user.getUuid());
 
@@ -155,8 +162,10 @@ public class AuthService {
     }
 
     UUID userUuid = new UUID(jwtTokenProvider.getUserUuidFromToken(token));
-    User user = Optional.ofNullable(userMapper.selectByUuid(userUuid.toString()))
-        .orElseThrow(() -> new UsernameNotFoundException("User not found with uuid: " + userUuid));
+    User user =
+        Optional.ofNullable(userMapper.selectByUuid(userUuid.toString()))
+            .orElseThrow(
+                () -> new UsernameNotFoundException("User not found with uuid: " + userUuid));
 
     return UserResponse.builder()
         .id(user.getId())
