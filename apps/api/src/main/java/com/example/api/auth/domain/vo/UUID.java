d4085317d57a9f6ee6f86b4baca12ba541bbb8c7
tech-takkwatanabe <tech.takkwatanabe@gmail.com>
2025-05-30 11:04:22 +0900

    🎨 Formatting

--- apps/api/src/main/java/com/example/api/auth/domain/vo/UUID.java
+++ apps/api/src/main/java/com/example/api/auth/domain/vo/UUID.java
@@ -34,10 +34,8 @@ public class UUID {
 
   @Override
   public boolean equals(Object o) {
-    if (this == o)
-      return true;
-    if (o == null || getClass() != o.getClass())
-      return false;
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
     UUID uuid = (UUID) o;
     return value.equals(uuid.value);
   }
